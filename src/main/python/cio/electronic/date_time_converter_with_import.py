907a6b92c9aa05e79883611e49cca24f4569af26
javathought <pascal.abaziou@gmail.com>
2018-09-27 17:14:12 +0200

    checkio(electronic) : date_time_converter
    solution with import of date_time
    solution without import of date_time

---
+++ src/main/python/cio/electronic/date_time_converter_with_import.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+def date_time(time: str) -> str:
+    dt = datetime.strptime(time, '%d.%m.%Y %H:%M')
+    return dt.strftime(f'%d %B %Y year %H hour{(dt.hour != 1)*"s"} %M minute{(dt.minute != 1)*"s"}').lstrip("0").replace(" 0", " ")
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(date_time('01.01.2000 00:00'))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert date_time("01.01.2000 00:00") == "1 January 2000 year 0 hours 0 minutes", "Millenium"
+    assert date_time("09.05.1945 06:30") == "9 May 1945 year 6 hours 30 minutes", "Victory"
+    assert date_time("20.11.1990 03:55") == "20 November 1990 year 3 hours 55 minutes", "Somebody was born"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
